6fab988eb8c1248ede90280ae34f31eff0c456df
sigeri-official <gergelysitku@gmail.com>
2025-07-31 13:26:21 +0200

    Finish Two_Sum

--- Leetcode/1_Easy/1_Two_Sum.py
+++ Leetcode/1_Easy/1_Two_Sum.py
@@ -1,3 +1,9 @@
 class Solution:
     def twoSum(self, nums, target):
-        pass
+        for i in range(len(nums)):  # Iterate for the first number
+            for j in range(i+1, len(nums)):  # Iterate for the second number
+                if nums[i] + nums[j] == target:
+                    return [i, j]  # Return the values
+
+sol = Solution()
+print(sol.twoSum(nums=[2,7,11,15], target=9))  # Call twoSum def
